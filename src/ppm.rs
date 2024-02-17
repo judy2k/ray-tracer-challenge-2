896766462271ba3e2d8b81fab91ae66010cb4a44
Judy2k <judy@judy.co.uk>
2024-02-17 17:57:00 +0000

    PPM implementation complete.

--- src/ppm.rs
+++ src/ppm.rs
@@ -1,12 +1,45 @@
 use crate::canvas::Canvas;
+use std::fmt::Write as FormatWrite;
 use std::io::{prelude::*, Result};
 
+fn clamp_int(f: f64) -> u16 {
+    match (f * 255_f64).round() {
+        v if v < 0. => 0,
+        v if v > 255. => 255,
+        v => v as u16,
+    }
+}
+
 impl Canvas {
-    fn write_ppm(&self, sink: &mut impl Write) -> Result<()> {
+    pub fn write_ppm(&self, sink: &mut impl Write) -> Result<()> {
         writeln!(sink, "P3")?;
         writeln!(sink, "{} {}", self.width, self.height)?;
         writeln!(sink, "255")?;
-        //sink.flush()?;
+
+        for row in 0..self.height {
+            let mut tokens = vec![];
+            for col in 0..self.width {
+                let pixel = self.pixel_at(col, row);
+                tokens.push(clamp_int(pixel.red()).to_string());
+                tokens.push(clamp_int(pixel.green()).to_string());
+                tokens.push(clamp_int(pixel.blue()).to_string());
+            }
+            let mut line = String::new();
+            for token in tokens {
+                if line.len() + token.len() + 2 > 70 {
+                    writeln!(sink, "{}", line)?;
+                    line = String::new();
+                }
+                if line.len() > 0 {
+                    write!(line, " {}", token).unwrap();
+                } else {
+                    write!(line, "{}", token).unwrap();
+                }
+            }
+            if line.len() > 0 {
+                writeln!(sink, "{}", line)?;
+            }
+        }
 
         Ok(())
     }
@@ -16,25 +49,17 @@ impl Canvas {
 mod test {
     use std::str::from_utf8;
 
-    use crate::{
-        canvas::Canvas,
-        color::{self, Color},
-    };
+    use crate::{canvas::Canvas, color::Color};
 
     #[test]
     fn test_header() {
         let c = Canvas::new(5, 3);
         let mut bytes = Vec::new();
         c.write_ppm(&mut bytes).unwrap();
-        //let s: String = String::from_utf8(bytes).unwrap();
-
-        assert_eq!(
-            bytes,
-            b"P3
-5 3
-255
-"
-        )
+        let lines: Vec<_> = from_utf8(&bytes).unwrap().lines().collect();
+        assert_eq!(lines[0], "P3");
+        assert_eq!(lines[1], "5 3");
+        assert_eq!(lines[2], "255");
     }
 
     #[test]
@@ -46,11 +71,60 @@ mod test {
 
         c.write_pixel(0, 0, c1);
         c.write_pixel(2, 1, c2);
-        c.write_pixel(4, 1, c3);
+        c.write_pixel(4, 2, c3);
 
         let mut bytes = Vec::new();
         c.write_ppm(&mut bytes).unwrap();
         let lines: Vec<_> = from_utf8(&bytes).unwrap().lines().collect();
+        println!("{:?}", from_utf8(&bytes).unwrap());
+        assert_eq!(lines[0], "P3");
+        assert_eq!(lines[1], "5 3");
+        assert_eq!(lines[2], "255");
         assert_eq!(lines[3], "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0");
+        assert_eq!(lines[4], "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0");
+        assert_eq!(lines[5], "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255");
+    }
+
+    #[test]
+    fn test_ppm_line_wrap() {
+        let mut canvas = Canvas::new(10, 2);
+        let c = Color::new(1., 0.8, 0.6);
+        for row in 0..canvas.height {
+            for col in 0..canvas.width {
+                canvas.write_pixel(col, row, c);
+            }
+        }
+
+        let mut bytes = Vec::new();
+        canvas.write_ppm(&mut bytes).unwrap();
+        let lines: Vec<_> = from_utf8(&bytes).unwrap().lines().collect();
+        assert_eq!(lines[0], "P3");
+        assert_eq!(lines[1], "10 2");
+        assert_eq!(lines[2], "255");
+        assert_eq!(
+            lines[3],
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
+        );
+        assert_eq!(
+            lines[4],
+            "153 255 204 153 255 204 153 255 204 153 255 204 153"
+        );
+        assert_eq!(
+            lines[5],
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
+        );
+        assert_eq!(
+            lines[6],
+            "153 255 204 153 255 204 153 255 204 153 255 204 153"
+        );
+    }
+
+    #[test]
+    fn test_ppm_ends_with_eol() {
+        let canvas = Canvas::new(5, 3);
+        let mut bytes = Vec::new();
+        canvas.write_ppm(&mut bytes).unwrap();
+        let last_char = from_utf8(&bytes).unwrap().chars().rev().next();
+        assert_eq!(last_char, Some('\n'));
     }
 }
