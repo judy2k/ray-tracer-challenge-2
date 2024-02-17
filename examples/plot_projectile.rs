a811f537f5188c7dbcf0ff34c5667b98a65733ee
Judy2k <judy@judy.co.uk>
2024-02-17 20:47:57 +0000

    I plotted a projectile!

---
+++ examples/plot_projectile.rs
@@ -0,0 +1,56 @@
+use ray_tracer_challenge_2::canvas::Canvas;
+use ray_tracer_challenge_2::color::Color;
+use ray_tracer_challenge_2::space::Tuple;
+use std::error::Error;
+use std::fs::OpenOptions;
+use std::io::BufWriter;
+
+#[derive(Debug)]
+struct Env {
+    gravity: Tuple,
+    wind: Tuple,
+}
+
+#[derive(Debug)]
+struct Projectile {
+    position: Tuple,
+    velocity: Tuple,
+}
+
+fn tick(env: &Env, projectile: Projectile) -> Projectile {
+    Projectile {
+        position: projectile.position + projectile.velocity,
+        velocity: projectile.velocity + env.gravity + env.wind,
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let projectile_color = Color::new(1.0, 0., 0.);
+
+    let mut projectile = Projectile {
+        position: Tuple::point(0.0, 1.0, 0.0),
+        velocity: Tuple::vector(1.0, 1.8, 0.0).normalize() * 11.25,
+    };
+    let environment = Env {
+        gravity: Tuple::vector(0.0, -0.1, 0.0),
+        wind: Tuple::vector(-0.01, 0.0, 0.0),
+    };
+
+    let mut canvas = Canvas::new(900, 550);
+
+    while projectile.position.y() >= 1.0 {
+        let canvas_x = projectile.position.x().round() as usize;
+        let canvas_y = canvas.height - projectile.position.y().round() as usize;
+
+        canvas.write_pixel(canvas_x, canvas_y, projectile_color);
+
+        projectile = tick(&environment, projectile);
+    }
+
+    let mut file = BufWriter::new(OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open("output/projectile.ppm")?);
+    canvas.write_ppm(&mut file)?;
+    Ok(())
+}
