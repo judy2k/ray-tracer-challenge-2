7654043aabf5c1e31d3b6f23414c6d9168c3a458
Judy2k <judy@judy.co.uk>
2024-04-08 12:34:45 +0100

    About to parallelise ray calculations.

--- examples/projection.rs
+++ examples/projection.rs
@@ -1,11 +1,7 @@
-use std::{error::Error, fs::OpenOptions, io::BufWriter};
+use std::{error::Error, fs::OpenOptions, io::BufWriter, time::Instant};
 
 use ray_tracer_challenge_2::{
-    canvas::Canvas,
-    color::Color,
-    ray::{Intersections, Ray},
-    shape::Sphere,
-    space::Tuple,
+    canvas::Canvas, color::Color, matrix::Matrix, ray::{Intersections, Ray}, shape::Sphere, space::Tuple
 };
 
 const OUTPUT_PATH: &str = "output/projection.ppm";
@@ -15,13 +11,16 @@ fn main() -> Result<(), Box<dyn Error>>{
     let wall_z = 10.0;
     let wall_size = 7.0;
 
-    let canvas_pixels = 1000;
+    let canvas_pixels = 180;
     let pixel_size = wall_size / canvas_pixels as f64;
     let half = wall_size / 2.0;
 
     let mut canvas = Canvas::new(canvas_pixels as usize, canvas_pixels as usize);
     let color = Color::new(1.0, 0.0, 0.0);
-    let shape = Sphere::new();
+    let mut shape = Sphere::new();
+    *shape.transformation() = Matrix::scaling(0.5, 1.0, 1.0);
+
+    let before = Instant::now();
 
     for y in 0..canvas_pixels {
         let world_y = half - pixel_size * y as f64;
@@ -31,6 +30,8 @@ fn main() -> Result<(), Box<dyn Error>>{
             let position = Tuple::point(world_x, world_y, wall_z);
 
             let r = Ray::new(origin, (position - origin).normalize());
+            // TODO: Optimize this - shouldn't transfer from vector to Intersections.
+            // Pass in &mut Intersections?
             let xs = shape.intersect(r);
             let mut is = Intersections::new();
             for x in xs.iter() {
@@ -41,6 +42,7 @@ fn main() -> Result<(), Box<dyn Error>>{
             }
         }
     }
+    println!("Generated pixels in {:.2?}", before.elapsed());
     let mut file = BufWriter::new(
         OpenOptions::new()
             .write(true)
